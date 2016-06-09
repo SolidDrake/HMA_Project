aacbbd8d3cc1f589bcda779f4f8acaeb7c63a784
Davide Donadio <Davide Donadio>
2016-06-09 17:12:09 +0200

    Minor Improvement

--- WEBSITE/Scripts/bootstrap.js
+++ WEBSITE/Scripts/bootstrap.js
@@ -2361,3 +2361,10 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(300);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(300);
+});
